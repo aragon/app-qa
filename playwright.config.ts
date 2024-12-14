e5ffb24ad6c9391264ae814e978ea71e9786a1fe
Barukimang <barukimang@gmail.com>
2024-12-14 19:57:31 +0300

    edits4

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
         ...devices["Desktop Chrome"],
         baseURL, // Pass the dynamically set baseURL to this project
       },
-      timeout: 60000,
+      timeout: 180000,
     },
     {
       name: "firefox",
