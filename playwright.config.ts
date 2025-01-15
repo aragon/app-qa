34a413c6bbc0c687430fc3bca3eae6d9eb463350
Barukimang <barukimang@gmail.com>
2025-01-15 15:56:27 +0300

    change timeout overall

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
         ...devices["Desktop Chrome"],
         baseURL, // Pass the dynamically set baseURL to this project
       },
-      timeout: 180000, // 3 minutes
+      timeout: 60000, // 1 minutes
     },
     {
       name: "firefox",
