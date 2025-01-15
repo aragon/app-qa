e191e83264f72a0dc5ac72ad8af11065acc2901a
Barukimang <barukimang@gmail.com>
2025-01-15 12:15:53 +0300

    cleanup

--- tests/multisig-plugin/02_publishProposal2.spec.ts
+++ tests/multisig-plugin/02_publishProposal2.spec.ts
@@ -7,46 +7,16 @@ const test = testWithSynpress(metaMaskFixtures(basicSetup));
 
 const { expect } = test;
 
-// test("should connect wallet to the MetaMask Test Dapp", async ({
-//   context,
-//   page,
-//   metamaskPage,
-//   extensionId,
-// }) => {
-//   const metamask = new MetaMask(
-//     context,
-//     metamaskPage,
-//     basicSetup.walletPassword,
-//     extensionId
-//   );
-
-//   await page.goto("/");
-
-//   await page.locator("#connectButton").click();
-//   await metamask.connectToDapp();
-//   await expect(page.locator("#accounts")).toHaveText(
-//     "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
-//   );
-
-//   await page.locator("#getAccounts").click();
-//   await expect(page.locator("#getAccountsResult")).toHaveText(
-//     "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
-//   );
-// });
-
 test("Publish signaling Proposal", async ({ page, metamask }) => {
   // Use a relative URL. The baseURL will be automatically prepended.
   await page.goto(
-    // "/dao/ethereum-sepolia-0xAC592AbEEee8812C23375fff6621540e8b861328/dashboard"
-    "/dao/ethereum-sepolia-0xB161EA70583f066cA00f62E9A749a9eBC1c807A8/dashboard"
+    // "/dao/ethereum-sepolia-0xAC592AbEEee8812C23375fff6621540e8b861328/dashboard" //multisig dao
+    "/dao/ethereum-sepolia-0xB161EA70583f066cA00f62E9A749a9eBC1c807A8/dashboard" //spp dao
   );
 
   await page.getByRole("button", { name: "Connect" }).click();
   await page.waitForTimeout(3000);
   await page.getByRole("button", { name: "Connect" }).click();
-  // await page.waitForTimeout(3000);
-  // await page.getByRole("button", { name: "MetaMask MetaMask" }).click();
-  // await page
   //   .getByRole("button", { name: "MetaMask MetaMask" })
   //   .click({ timeout: 5000 });
   await page
