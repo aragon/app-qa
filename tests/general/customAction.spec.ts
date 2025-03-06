6fca41e73432097d2b4f3238729efc0b175af449
Barukimang <barukimang@gmail.com>
2025-03-06 17:41:54 +0300

    test proposal with custom action

--- tests/general/customAction.spec.ts
+++ tests/general/customAction.spec.ts
@@ -1,11 +1,11 @@
 import { testWithSynpress } from "@synthetixio/synpress";
 import { metaMaskFixtures } from "@synthetixio/synpress/playwright";
-import basicSetup from "../../helpers/wallet.setup";
-import { connectWallet } from "../../helpers/connectWallet";
+import basicSetup from "../helpers/wallet.setup";
+import { connectWallet } from "../helpers/connectWallet";
 
 const test = testWithSynpress(metaMaskFixtures(basicSetup));
 
-test("Admin Plugin Test - Publish Proposal", async ({ page, metamask }) => {
+test("General Test - Add Custom Action", async ({ page, metamask }) => {
   await connectWallet(page, metamask);
   await page.goto(
     "/dao/ethereum-sepolia-0x1EBc713b156e4eF97e77291dbe9dF9F762D73404/dashboard" //adminPlugin DAO
@@ -17,8 +17,23 @@ test("Admin Plugin Test - Publish Proposal", async ({ page, metamask }) => {
     .or(page.getByRole("link", { name: "Proposal", exact: true }))
     .click();
   await page.getByPlaceholder("Type a title").click();
-  await page.getByPlaceholder("Type a title").fill("Create proposal");
+  await page
+    .getByPlaceholder("Type a title")
+    .fill("Create proposal with custom action");
   await page.getByRole("button", { name: "Next" }).click();
+  await page.getByRole("button", { name: "Custom" }).click();
+  await page.getByPlaceholder("ENS or 0x …").click();
+  await page
+    .getByPlaceholder("ENS or 0x …")
+    .fill("0x0b0e0c2e07324fd29e9b21d48af43c9a30f4c5da");
+  await page.getByRole("button", { name: "Add contract" }).click();
+  await page.getByRole("option", { name: "approve" }).first().click();
+  await page.getByPlaceholder("address", { exact: true }).click();
+  await page
+    .getByPlaceholder("address", { exact: true })
+    .fill("0x0b0e0c2e07324fd29e9b21d48af43c9a30f4c5da");
+  await page.getByPlaceholder("uint256").click();
+  await page.getByPlaceholder("uint256").fill("1000");
   await page.getByRole("button", { name: "Publish proposal" }).click();
   await page.getByRole("button", { name: "Approve transaction" }).click();
   await page.waitForTimeout(3000);
