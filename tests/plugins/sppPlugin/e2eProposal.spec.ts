ebad41406d2bfb282d393223d0f63511a5f80829
Barukimang <barukimang@gmail.com>
2025-03-07 17:50:42 +0300

    e2e proposal test at spp plugin

---
+++ tests/plugins/sppPlugin/e2eProposal.spec.ts
@@ -0,0 +1,69 @@
+import { testWithSynpress } from "@synthetixio/synpress";
+import { metaMaskFixtures } from "@synthetixio/synpress/playwright";
+import basicSetup from "../../helpers/wallet.setup";
+import { connectWallet } from "../../helpers/connectWallet";
+
+const test = testWithSynpress(metaMaskFixtures(basicSetup));
+
+test("Spp Plugin Test - Publish, Approve, and Execute Proposal", async ({
+  page,
+  metamask,
+}) => {
+  await connectWallet(page, metamask);
+  await page.goto(
+    "/dao/ethereum-sepolia-0xf2Fe4187dA24181C03CdB3cb59C52Cfc45AF8D9f/dashboard" //sppPlugin DAO
+  );
+  await page.getByRole("link", { name: "Proposals" }).click();
+  await page.waitForTimeout(3000);
+  await page
+    .getByRole("button", { name: "Proposal", exact: true })
+    .or(page.getByRole("link", { name: "Proposal", exact: true }))
+    .click();
+  await page.waitForTimeout(3000);
+  await page.getByRole("button", { name: "End To End ETE" }).click();
+  await page.getByRole("button", { name: "Create" }).click();
+  await page.getByPlaceholder("Type a title").click();
+  await page.getByRole("button", { name: "Next" }).click();
+  await page
+    .getByPlaceholder("Type a title")
+    .fill("Publish, Approve, and Execute Proposal");
+  await page.getByRole("button", { name: "Next" }).click();
+  await page.getByRole("button", { name: "Action" }).click();
+  await page.getByRole("option", { name: "Transfer" }).click();
+  await page.getByPlaceholder("ENS or 0x …").click();
+  await page
+    .getByPlaceholder("ENS or 0x …")
+    .fill("0xf2Fe4187dA24181C03CdB3cb59C52Cfc45AF8D9f");
+  await page.getByRole("button", { name: "Select" }).click();
+  await page.getByRole("button", { name: "Select" }).click({ force: true });
+  await page.getByRole("button", { name: /ETH/ }).click();
+  await page.getByPlaceholder("0", { exact: true }).click();
+  await page.getByPlaceholder("0", { exact: true }).fill("0.01234");
+  await page.getByRole("button", { name: "Next" }).click();
+  await page.getByRole("button", { name: "Publish proposal" }).click();
+  await page.getByRole("button", { name: "Approve transaction" }).click();
+  await page.waitForTimeout(3000);
+  await metamask.confirmTransaction();
+  await page.waitForTimeout(3000);
+  await page.getByRole("link", { name: "View proposal" }).click();
+  await page.waitForTimeout(10000);
+  await page.getByRole("tab", { name: "End To End" }).click();
+  await page
+    .getByRole("link", { name: /Publish, Approve, and Execute Proposal/ })
+    .first()
+    .click();
+  await page.waitForTimeout(3000);
+  await page.getByRole("button", { name: "Vote on proposal" }).click();
+  await page.getByLabel("Yes").click();
+  await page.getByRole("button", { name: "Submit vote" }).click();
+  await page.getByRole("button", { name: "Approve transaction" }).click();
+  await page.waitForTimeout(3000);
+  await metamask.confirmTransaction();
+  await page.getByRole("button", { name: "View proposal" }).click();
+  await page.waitForTimeout(5000);
+  await page.getByRole("button", { name: "Execute" }).click();
+  await page.getByRole("button", { name: "Approve transaction" }).click();
+  await page.waitForTimeout(3000);
+  await metamask.confirmTransaction();
+  await page.getByRole("button", { name: "View proposal" }).click();
+});
