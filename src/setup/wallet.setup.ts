d7918cd7b90e32dfe96b9262502a101e7855a315
Ruggero Cino <ruggero.cino@gmail.com>
2025-04-05 19:23:11 +0200

    Update wallet.setup.ts

--- src/setup/wallet.setup.ts
+++ src/setup/wallet.setup.ts
@@ -1,5 +1,5 @@
 import { defineWalletSetup } from '@synthetixio/synpress';
-import { MetaMask } from '@synthetixio/synpress/playwright';
+import { getExtensionId, MetaMask } from '@synthetixio/synpress/playwright';
 import 'dotenv/config';
 import { networkDefinitions } from '../helpers';
 
@@ -7,7 +7,8 @@ const SEED_PHRASE = process.env.METAMASK_SEED_PHRASE!;
 const PASSWORD = process.env.METAMASK_PASSWORD!;
 
 const walletSetup = defineWalletSetup(PASSWORD, async (context, walletPage) => {
-    const metamask = new MetaMask(context, walletPage, PASSWORD);
+    const extensionId = await getExtensionId(context, 'MetaMask');
+    const metamask = new MetaMask(context, walletPage, PASSWORD, extensionId);
     await metamask.importWallet(SEED_PHRASE);
 
     const walletSetupList = Object.values(networkDefinitions).filter((network) => network.needsSetup);
