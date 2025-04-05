85eae7046d9ce5d86c9a068319ce54845d9cee92
Ruggero Cino <ruggero.cino@gmail.com>
2025-04-05 18:44:09 +0200

    Update wallet.setup.ts

--- src/setup/wallet.setup.ts
+++ src/setup/wallet.setup.ts
@@ -5,10 +5,11 @@ import 'dotenv/config';
 const SEED_PHRASE = process.env.METAMASK_SEED_PHRASE!;
 const PASSWORD = process.env.METAMASK_PASSWORD!;
 
+console.log(PASSWORD);
+
 const walletSetup = defineWalletSetup(PASSWORD, async (context, walletPage) => {
     const metamask = new MetaMask(context, walletPage, PASSWORD);
     await metamask.importWallet(SEED_PHRASE);
-    console.log(PASSWORD);
 
     /*
     const walletSetupList = Object.values(networkDefinitions).filter((network) => network.needsSetup);
